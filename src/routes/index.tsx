ec438ccb8741225032b5e0d1f0aea64e5e617233
Hyun Sang <pycandyq@naver.com>
2024-12-12 19:21:40 +0900

    redux-toolkit store 설정 및 테스트 완료

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from '../pages/MainPage/Main';
+import NotFoundPage from '../pages/NotFoundPage/NotFound';
+import RoutePaths from './RoutePath';
+
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path={RoutePaths.MAIN} element={<MainPage />} />
+    
+        {/* 404 페이지 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
