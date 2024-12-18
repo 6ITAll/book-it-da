3fd41918610ff6a8cb57f03cca3f2354a7f1e529
publdaze <publdaze@naver.com>
2024-12-18 09:34:01 +0900

    :lipstick:feat: set TabSection Structure
    Related to: #22

--- src/components/MyPage/TabSection.tsx
+++ src/components/MyPage/TabSection.tsx
@@ -1,5 +1,21 @@
+import { Stack, Tab, Tabs } from '@mui/material';
+
 const TabSection = (): JSX.Element => {
-  return <div>TabSection</div>;
+  const tabs = [
+    { id: 1, label: '내 서재' },
+    { id: 2, label: '내 피드' },
+    { id: 2, label: '북마크 피드' },
+  ];
+
+  return (
+    <Stack>
+      <Tabs>
+        {tabs.map(({ id, label }) => (
+          <Tab key={id} label={label} />
+        ))}
+      </Tabs>
+    </Stack>
+  );
 };
 
 export default TabSection;
