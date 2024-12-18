ad83d97c7f99cd04e0ac17c876aeb18aa3866d08
deenuit113 <leejh5314@gmail.com>
2024-12-18 23:28:32 +0900

    :sparkles:feat: add feed page 무한스크롤과 masonry layout 구현 Related to: #24

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,10 +1,61 @@
-import './Page.css';
+import { useState } from 'react';
+import { CircularProgress, Container } from '@mui/material';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { Post } from '@/shared/types/type';
 
-const Main: React.FC = () => {
+const Main = (): JSX.Element => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  // Mock Posts 생성 > 추후 API 요청으로 수정
+  const fetchMoreData = () => {};
+
   return (
-    <div className="page">
-      <h2>피드 페이지</h2>
-    </div>
+    <Container maxWidth="md">
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <div
+            style={{
+              height: '50px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              border: '1px solid red',
+              padding: '1rem 0',
+            }}
+          >
+            <CircularProgress color="primary" value={25} />
+          </div>
+        }
+        endMessage={
+          <div
+            style={{
+              height: '50px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              border: '1px solid red',
+              padding: '1rem 0',
+            }}
+          >
+            <p>더이상 표시할 것이 없습니다.</p>
+          </div>
+        }
+        style={{ paddingBottom: '16px' }}
+      >
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry gutter="16px">
+            {posts.map((post) => (
+              <div>카드</div>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </InfiniteScroll>
+    </Container>
   );
 };
 
