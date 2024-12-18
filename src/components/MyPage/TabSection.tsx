77405565ca39c08562e966af461e8f5e1d0ca495
publdaze <publdaze@naver.com>
2024-12-18 09:49:38 +0900

    :sparkles:feat: add tab event handler
    Related to: #22

--- src/components/MyPage/TabSection.tsx
+++ src/components/MyPage/TabSection.tsx
@@ -1,19 +1,23 @@
 import { Stack, Tab, Tabs } from '@mui/material';
+import { useState } from 'react';
 
 const TabSection = (): JSX.Element => {
   const tabs = [
-    { id: 1, label: '내 서재' },
-    { id: 2, label: '내 피드' },
-    { id: 2, label: '북마크 피드' },
+    { id: 1, label: '내 서재', component: <></> },
+    { id: 2, label: '내 피드', component: <></> },
+    { id: 2, label: '북마크 피드', component: <></> },
   ];
+
+  const [tab, setTab] = useState(0);
 
   return (
     <Stack>
-      <Tabs>
+      <Tabs value={tab} onChange={(_, newValue) => setTab(newValue)}>
         {tabs.map(({ id, label }) => (
           <Tab key={id} label={label} />
         ))}
       </Tabs>
+      {tabs.map(({ component }, index) => tab === index && component)}
     </Stack>
   );
 };
