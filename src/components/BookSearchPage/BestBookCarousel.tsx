278dbd548cc25c83329b0467108429378878390b
Hyun Sang <pycandyq@naver.com>
2024-12-19 17:56:58 +0900

    ♻️refactor: Used as day.js

--- src/components/BookSearchPage/BestBookCarousel.tsx
+++ src/components/BookSearchPage/BestBookCarousel.tsx
@@ -1,18 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { Typography } from '@mui/material';
 import { css } from '@emotion/react';
+import dayjs from 'dayjs';
 import { useGetBestBooksQuery } from '@/features/BookSearchPage/api/bestBookGetApi';
-import Carousel from '@/components/common/Carousel';
+import Carousel from '@/components/commons/Carousel';
 import BestBookCard from '@/components/BookSearchPage/BestBookCard';
 
 const BestBookCarousel = (): JSX.Element => {
   const { data, isLoading, error } = useGetBestBooksQuery();
-
   // 주차 정보 추출 (현재 날짜 기준)
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentWeek = Math.ceil(currentDate.getDate() / 7);
+  const currentDate = dayjs();
+  const currentYear = currentDate.year();
+  const currentMonth = currentDate.month() + 1; // day.js는 0부터 시작
+  const firstDayOfMonth = currentDate.startOf('month');
+  const weekNumber = Math.ceil(
+    (currentDate.date() + firstDayOfMonth.day()) / 7,
+  );
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생하였습니다.</div>;
@@ -27,14 +30,14 @@ const BestBookCarousel = (): JSX.Element => {
         `}
         gutterBottom
       >
-        {`${currentYear}년 ${currentMonth}월 ${currentWeek}주차 기준`}
+        {`${currentYear}년 ${currentMonth}월 ${weekNumber}주차 기준`}
       </Typography>
       <Carousel>
         {data?.item?.map((book) => (
           <BestBookCard
             key={book.itemId}
             image={book.cover}
-            link={book.link}
+            onClick={() => console.log('상세 페이지 이동')}
             title={book.title}
           />
         ))}
