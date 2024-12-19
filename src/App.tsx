4f72d5ff12769fd5da871f71d46c6d4f5ee0d5d5
Hyun Sang <pycandyq@naver.com>
2024-12-19 09:38:26 +0900

    💄design: Edit mypage path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LoginSignup from './pages/MainPage/Login';
 import FeedPage from './pages/MainPage/Main';
 import BookSearchPage from './pages/BookSearchPage/BookSearch';
-import MyPage from './pages/MainPage/Mypage';
+import MyPage from './pages/MyPage/MyPage';
 import './App.css';
 
 const App: React.FC = () => {
