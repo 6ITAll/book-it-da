f65b6836857158019f62989f487f820b505bd95d
publdaze <publdaze@naver.com>
2024-12-19 10:26:14 +0900

    :sparkles:feat: change follower following text to button
    Related to: #37

--- src/components/MyPage/UserInfoSummary.tsx
+++ src/components/MyPage/UserInfoSummary.tsx
@@ -1,13 +1,19 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
 
 interface UserInfoSummaryProps {
   count: number;
   label: string;
+  isAction?: boolean;
 }
 
-const UserInfoSummary = ({ count, label }: UserInfoSummaryProps) => {
+const UserInfoSummary = ({ count, label, isAction }: UserInfoSummaryProps) => {
   return (
-    <Stack alignItems="center">
+    <Stack
+      component={isAction ? Button : 'div'}
+      alignItems="center"
+      padding="6px 4px"
+      minWidth={64}
+    >
       <Typography>{count}</Typography>
       <Typography>{label}</Typography>
     </Stack>
