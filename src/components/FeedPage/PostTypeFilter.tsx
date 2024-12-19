33c25c857dd22032c2dc9056371c870df7ca1b5e
deenuit113 <leejh5314@gmail.com>
2024-12-19 13:26:46 +0900

    :recycle: refactor feed page filtering buttons 피드페이지 필터링 버튼 컴포넌트로 분리 Related to: #24

---
+++ src/components/FeedPage/PostTypeFilter.tsx
@@ -0,0 +1,32 @@
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { PostType } from '@/shared/types/type';
+
+interface PostTypeFilterProps {
+  postType: PostType | '';
+  onPostTypeChange: (
+    event: React.MouseEvent<HTMLElement>,
+    newValue: PostType | '',
+  ) => void;
+}
+
+export const PostTypeFilter = ({
+  postType,
+  onPostTypeChange,
+}: PostTypeFilterProps) => {
+  return (
+    <ToggleButtonGroup value={postType} exclusive onChange={onPostTypeChange}>
+      <ToggleButton
+        value="한줄평"
+        sx={{ padding: '5px 10px', fontSize: '12px' }}
+      >
+        한줄평
+      </ToggleButton>
+      <ToggleButton
+        value="포스팅"
+        sx={{ padding: '5px 10px', fontSize: '12px' }}
+      >
+        포스팅
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
