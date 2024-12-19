7b3379c4e66a30d1fee61f4881e04541d6c904b3
publdaze <publdaze@naver.com>
2024-12-19 10:26:14 +0900

    :sparkles:feat: change follower following text to button
    Related to: #37

--- src/components/MyPage/UserInfoSection.tsx
+++ src/components/MyPage/UserInfoSection.tsx
@@ -9,8 +9,8 @@ const UserInfoSection = (): JSX.Element => {
     about: '책을 사랑하는 독서가',
     userStats: [
       { count: 286, label: '피드' },
-      { count: 842, label: '팔로워' },
-      { count: 267, label: '팔로잉' },
+      { count: 842, label: '팔로워', isAction: true },
+      { count: 267, label: '팔로잉', isAction: true },
     ],
   };
 
@@ -21,13 +21,18 @@ const UserInfoSection = (): JSX.Element => {
         src={user.avartarUrl}
         sx={{ width: 100, height: 100 }}
       />
-      <Stack spacing={2}>
+      <Stack spacing={1}>
         <Typography variant="h6" fontWeight="bold">
           {user.name}
         </Typography>
-        <Stack direction="row" spacing={4}>
-          {user?.userStats?.map(({ count, label }) => (
-            <UserInfoSummary key={label} count={count} label={label} />
+        <Stack direction="row" spacing={2} alignItems="center">
+          {user?.userStats?.map(({ count, label, isAction }) => (
+            <UserInfoSummary
+              key={label}
+              count={count}
+              label={label}
+              isAction={isAction}
+            />
           ))}
         </Stack>
         <Typography variant="body2" color="grey.700">
