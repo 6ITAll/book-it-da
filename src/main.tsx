fe0a40372bf1ebe4e7bca47d62208fe9ca8d93e1
publdaze <publdaze@naver.com>
2024-12-24 00:23:24 +0900

    💄design: add MUI theme privider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { store } from './store/index.tsx';
 import { Provider } from 'react-redux';
+import { muiTheme } from '@styles/theme.ts';
+import { ThemeProvider } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={muiTheme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
 );
