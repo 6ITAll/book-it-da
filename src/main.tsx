eddf90a9a69999aba8e405f2f8fefdd39c458886
publdaze <publdaze@naver.com>
2024-12-25 08:36:37 +0900

    📦chore: set up Mock Service Worker in the browser
    Related to: #54

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,22 @@ import { Provider } from 'react-redux';
 import { muiTheme } from '@styles/theme.ts';
 import { ThemeProvider } from '@mui/material';
 
-createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <ThemeProvider theme={muiTheme}>
-      <App />
-    </ThemeProvider>
-  </Provider>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+      <ThemeProvider theme={muiTheme}>
+        <App />
+      </ThemeProvider>
+    </Provider>,
+  );
+});
