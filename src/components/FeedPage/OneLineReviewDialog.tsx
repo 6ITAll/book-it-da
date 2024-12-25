29ea9152db2c898158457ac52d6b86ab02564f93
deenuit113 <leejh5314@gmail.com>
2024-12-25 17:17:25 +0900

    :sparkles:feat: add rating func oneline review Dialog 다이얼로그에 별점 기능 추가 상세페이지에서 들어갈 경우 책 검색 표시하지 않음 별점 컴포넌트 추가 Resolves: #56

--- src/components/FeedPage/OneLineReviewDialog.tsx
+++ src/components/FeedPage/OneLineReviewDialog.tsx
@@ -4,45 +4,81 @@ import { PostType } from './WriteDialog';
 import BookSearchAutoComplete from '@components/commons/BookSearchAutoComplete';
 import { Book } from '@shared/types/type';
 import HybridDialog from '@components/commons/HybridDialog';
+import StarRating from '@components/commons/StarRating';
 
 interface OneLineReviewDialogProps {
-  handleBack: () => void;
-  selectedType: PostType;
-  setSelectedType: React.Dispatch<React.SetStateAction<PostType>>;
+  // 포스팅 타입 선택 다이얼로그에서 오는 Props
+  handleBack?: () => void;
+  selectedType?: PostType;
+  setSelectedType?: React.Dispatch<React.SetStateAction<PostType>>;
+  // 상세 페이지에서 오는 Props
+  receivedBook?: Book;
+  receivedRating?: number;
+  isOpen?: boolean;
+  onClose?: () => void;
 }
 
 const OneLineReviewDialog = ({
   selectedType,
   setSelectedType,
   handleBack,
+  receivedBook,
+  receivedRating,
+  isOpen,
+  onClose,
 }: OneLineReviewDialogProps) => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const [selectedBook, setSelectedBook] = useState<Book | null>(
+    receivedBook || null,
+  );
   const [review, setReview] = useState('');
+  const [rating, setRating] = useState<number>(receivedRating || 0);
 
+  const dialogOpen = selectedType ? selectedType === 'review' : !!isOpen;
   // 다이얼로그 닫히면 책 검색 결과 초기화
   useEffect(() => {
-    if (selectedType !== 'review') {
+    if (!receivedBook && selectedType !== 'review') {
       setSelectedBook(null);
       setSearchQuery('');
       setReview('');
     }
-  }, [selectedType]);
+  }, [selectedType, receivedBook]);
+  // 상세 페이지에서 가져온 별점 적용
+  useEffect(() => {
+    if (receivedRating) {
+      setRating(receivedRating);
+    }
+  }, [receivedRating]);
 
   const handleSubmit = () => {
     // 한줄평 제출 로직 구현
     console.log('한줄평 제출:', { selectedBook, review });
   };
 
+  const handleDialogClose: React.Dispatch<
+    React.SetStateAction<boolean>
+  > = () => {
+    if (selectedType) {
+      // 포스팅 타입 선택 다이얼로그에서 연 경우
+      setSelectedType?.(null);
+    } else {
+      // 다른 경로로 다이얼로그를 연 경우
+      onClose?.();
+    }
+  };
+
   const contentNode = (
     <Stack sx={{ gap: '20px' }}>
-      <BookSearchAutoComplete
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-        selectedBook={selectedBook}
-        setSelectedBook={setSelectedBook}
-      />
-
+      {/* 상세 페이지에서 넘어갈 시 책 검색 기능 표시하지 않음 */}
+      {!receivedBook && (
+        <BookSearchAutoComplete
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          selectedBook={selectedBook}
+          setSelectedBook={setSelectedBook}
+        />
+      )}
+      {/* 한줄평 하고 있는 책 < 책 검색 or 책 상세 페이지 */}
       {selectedBook && (
         <Box>
           <Stack direction="row" spacing={2} alignItems="center">
@@ -54,12 +90,13 @@ const OneLineReviewDialog = ({
             <Stack>
               <Typography>{selectedBook.bookTitle}</Typography>
               <Typography variant="body2">{selectedBook.author}</Typography>
-              <Typography>{selectedBook.itemId}</Typography>
             </Stack>
           </Stack>
         </Box>
       )}
-
+      {/* 별점 기능 */}
+      <StarRating rating={rating} onRatingChange={setRating} isDialog={true} />
+      {/* 한줄평 작성 영역 */}
       <TextField
         fullWidth
         multiline
@@ -84,8 +121,8 @@ const OneLineReviewDialog = ({
 
   return (
     <HybridDialog
-      open={selectedType === 'review'}
-      setOpen={() => setSelectedType(null)}
+      open={dialogOpen}
+      setOpen={handleDialogClose}
       title="한줄평 작성"
       onBack={handleBack}
       action="한줄평 작성"
