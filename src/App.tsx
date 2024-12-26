cc64214586e1efe641c9cd986d076359855f4c5d
Hyun Sang <pycandyq@naver.com>
2024-12-27 00:25:06 +0900

    ✨feat: One-line review, post details view page router settings
    Related to: #70

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import BookSearchPage from './pages/BookSearchPage/BookSearch';
 import MyPage from './pages/MyPage/MyPage';
 import './App.css';
 import BookDetailPage from '@pages/BookDetailPage/BookDetailPage';
+import ReviewMorePage from '@pages/ReviewMorePage/ReviewMorePage';
+import PostMorePage from '@pages/PostMorePage/PostMorePage';
 
 const App: React.FC = () => {
   return (
@@ -46,6 +48,14 @@ const App: React.FC = () => {
               <Route path="/my-page/:userId?" element={<MyPage />} />
               <Route path="/" element={<FeedPage />} />
               <Route path="/bookDetail/:itemId" element={<BookDetailPage />} />
+              <Route
+                path="/bookDetail/:itemId/reviews"
+                element={<ReviewMorePage />}
+              />
+              <Route
+                path="/bookDetail/:itemId/posts"
+                element={<PostMorePage />}
+              />
             </Routes>
           </main>
         </div>
