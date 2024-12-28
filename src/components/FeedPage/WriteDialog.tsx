9f708875812a6a76386c2486fb595f115e3e6574
deenuit113 <leejh5314@gmail.com>
2024-12-28 14:25:43 +0900

    :lipstick:design: enhance post type select dialog 포스트 타입 선택 다이얼로그 버튼 변경 Related to: #78

--- src/components/FeedPage/WriteDialog.tsx
+++ src/components/FeedPage/WriteDialog.tsx
@@ -5,6 +5,7 @@ import PostingDialog from './PostingDialog';
 import OneLineReviewDialog from './OneLineReviewDialog';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 export type PostType = 'post' | 'review' | null;
 
@@ -30,19 +31,38 @@ const WriteDialog = ({
   };
 
   const contentNode = (
-    <Stack spacing={2}>
+    <Stack
+      spacing={0}
+      sx={{
+        '& .MuiButton-root': {
+          py: 2,
+          justifyContent: 'space-between',
+          backgroundColor: 'transparent',
+          border: 'none',
+          '&:hover': {
+            backgroundColor: '#e0e0e0',
+            border: 'none',
+          },
+          '& .MuiButton-startIcon': {
+            marginRight: 1, // 아이콘과 텍스트 사이 간격 줄임
+          },
+        },
+      }}
+    >
       <Button
         variant="outlined"
         fullWidth
         startIcon={<PostAddIcon />}
+        endIcon={<ChevronRightIcon />}
         onClick={() => handleTypeSelect('post')}
       >
-        포스트 작성
+        포스팅 작성
       </Button>
       <Button
         variant="outlined"
         fullWidth
         startIcon={<BorderColorIcon />}
+        endIcon={<ChevronRightIcon />}
         onClick={() => handleTypeSelect('review')}
       >
         한줄평 작성
