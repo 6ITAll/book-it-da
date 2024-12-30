006b34c9759723fae26ad2c672eb5e9c294de067
ph0109 <pf6350@gmail.com>
2024-12-30 16:05:28 +0900

    🎨refactor: Implement AppRouter and update layout - AppRouter를 사용하여 페이지 라우팅 설정 - 로그인 상태를 확인하여 프로필 아이콘 또는 로그인 버튼을 동적으로 표시하도록 구현 - 검색 입력 필드를 추가하고, 검색 시 /search 페이지로 리다이렉트하도록 설정 - 전반적인 레이아웃 개선 Related to: #71

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,12 +3,15 @@ import counterReducer from '../features/counter/counterSlice';
 import { bookSearchApi } from '@features/BookSearchPage/api/bookSearchApi';
 import { bestBookGetApi } from '@features/BookSearchPage/api/bestBookGetApi';
 import bookSearchReducer from '@features/BookSearchPage/Slice/bookSearchSlice';
+import userReducer from './userSlice/userSlice';
+
 export const store = configureStore({
   reducer: {
     bookSearch: bookSearchReducer,
     [bookSearchApi.reducerPath]: bookSearchApi.reducer,
     [bestBookGetApi.reducerPath]: bestBookGetApi.reducer,
     counter: counterReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
