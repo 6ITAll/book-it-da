d7e63576017141f2937ff012ee963923031a045d
deenuit113 <leejh5314@gmail.com>
2024-12-31 10:49:34 +0900

    :art:style: add theme configuration and provider 다크모드 고려한 커스텀 테마와 프로바이더 적용 Related to: #46

--- src/App.tsx
+++ src/App.tsx
@@ -5,56 +5,61 @@ import FeedPage from './pages/MainPage/Main';
 import BookSearchPage from './pages/BookSearchPage/BookSearch';
 import MyPage from './pages/MyPage/MyPage';
 import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { muiTheme } from '@styles/theme';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="app-container">
-        <header className="header">
-          <div className="logo">잇다</div>
-          <div className="search-bar">
-            <input type="text" placeholder="책 검색..." />
-            <button>검색</button>
-          </div>
-          <div className="login-button">
-            <Link to="/login-signup">로그인</Link>
-          </div>
-        </header>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Router>
+        <div className="app-container">
+          <header className="header">
+            <div className="logo">잇다</div>
+            <div className="search-bar">
+              <input type="text" placeholder="책 검색..." />
+              <button>검색</button>
+            </div>
+            <div className="login-button">
+              <Link to="/login-signup">로그인</Link>
+            </div>
+          </header>
 
-        <div className="main-content">
-          <aside className="sidebar">
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/feed">홈</Link>
-                </li>
-                <li>
-                  <Link to="/search">책 검색</Link>
-                </li>
-                <li>
-                  <Link to="/my-page">마이페이지</Link>
-                </li>
-              </ul>
-            </nav>
-          </aside>
+          <div className="main-content">
+            <aside className="sidebar">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/feed">홈</Link>
+                  </li>
+                  <li>
+                    <Link to="/search">책 검색</Link>
+                  </li>
+                  <li>
+                    <Link to="/my-page">마이페이지</Link>
+                  </li>
+                </ul>
+              </nav>
+            </aside>
 
-          <main className="content">
-            <Routes>
-              <Route path="/login-signup" element={<LoginSignup />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/feed" element={<FeedPage />} />
-              <Route path="/search" element={<BookSearchPage />} />
-              <Route path="/my-page/:userId?" element={<MyPage />} />
-              <Route path="/" element={<FeedPage />} />
-            </Routes>
-          </main>
+            <main className="content">
+              <Routes>
+                <Route path="/login-signup" element={<LoginSignup />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/feed" element={<FeedPage />} />
+                <Route path="/search" element={<BookSearchPage />} />
+                <Route path="/my-page/:userId?" element={<MyPage />} />
+                <Route path="/" element={<FeedPage />} />
+              </Routes>
+            </main>
+          </div>
+
+          <footer className="footer">
+            <p>&copy; 2024 육잇다 All rights reserved.</p>
+          </footer>
         </div>
-
-        <footer className="footer">
-          <p>&copy; 2024 육잇다 All rights reserved.</p>
-        </footer>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 };
 
