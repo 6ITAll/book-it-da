6c9eb2871c26b4ae200f5593a29071f44180a0d9
ph0109 <pf6350@gmail.com>
2025-01-02 23:23:45 +0900

    ✨ feat: configure Redux store with Kakao API integration - Redux 스토어를 설정하여 카카오 API 통합을 추가 - Kakao API 미들웨어를 포함하여 API 요청을 처리할 수 있도록 구성 Related to: #79

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,17 +3,21 @@ import counterReducer from '../features/counter/counterSlice';
 import { bookSearchApi } from '@features/BookSearchPage/api/bookSearchApi';
 import { bestBookGetApi } from '@features/BookSearchPage/api/bestBookGetApi';
 import bookSearchReducer from '@features/BookSearchPage/Slice/bookSearchSlice';
+import { kakaoApi } from '@features/SNSLogin/api/Kakaoapi';
+
 export const store = configureStore({
   reducer: {
     bookSearch: bookSearchReducer,
     [bookSearchApi.reducerPath]: bookSearchApi.reducer,
     [bestBookGetApi.reducerPath]: bestBookGetApi.reducer,
+    [kakaoApi.reducerPath]: kakaoApi.reducer,
     counter: counterReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       bookSearchApi.middleware,
       bestBookGetApi.middleware,
+      kakaoApi.middleware,
     ]),
 });
 
