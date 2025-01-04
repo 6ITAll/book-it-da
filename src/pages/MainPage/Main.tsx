c9595ccbf7981ed5135993fdd70532dc3cf7a654
deenuit113 <leejh5314@gmail.com>
2025-01-04 16:56:18 +0900

    :lipstick:design: modify masonry layout 피드페이지 메이슨리 레이아웃 카드 개수 변경

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -202,7 +202,7 @@ const Main = (): JSX.Element => {
           </Box>
         ) : (
           <Masonry
-            columns={{ xs: 1, sm: 2, md: 4 }}
+            columns={{ xs: 1, sm: 2, md: 3, lg: 5 }}
             spacing={4}
             sx={{
               width: '100%',
