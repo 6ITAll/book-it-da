5ac20d0677637eedce9a496b01660db928db74d9
ph0109 <pf6350@gmail.com>
2025-01-05 17:42:10 +0900

    ✨ feat: add Kakao OAuth token handler - 카카오 OAuth 토큰 요청을 위한 모의 핸들러를 구현 Related to: #102

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,9 +1,11 @@
 import { bookshelvesHandlers } from './bookshelves';
 import { libraryHandlers } from './library';
 import { postingHandlers } from './posting';
+import { kakaoHandlers } from './kakao';
 
 export const handlers = [
   ...libraryHandlers,
   ...bookshelvesHandlers,
   ...postingHandlers,
+  ...kakaoHandlers,
 ];
