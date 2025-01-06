71184973e75509edc433407f962c36b049bd41fc
Hyun Sang <pycandyq@naver.com>
2025-01-06 11:28:55 +0900

    🔧chore: Create post and library related handlers

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -4,12 +4,13 @@ import { postingHandlers } from './posting';
 import { genderAgeHandlers } from './genderAge';
 import { reviewHandlers } from './DetailPageReview';
 import { postHandlers } from './DetailPagePost';
-
+import { addToLibraryHandlers } from './addToLibrary';
 export const handlers = [
   ...libraryHandlers,
   ...bookshelvesHandlers,
+  ...postHandlers,
   ...postingHandlers,
   ...genderAgeHandlers,
   ...reviewHandlers,
-  ...postHandlers,
+  ...addToLibraryHandlers,
 ];
