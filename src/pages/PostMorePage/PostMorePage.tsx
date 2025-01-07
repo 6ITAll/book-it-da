2ad18faf010042029ab5714a43328d4ae98f7777
publdaze <publdaze@naver.com>
2025-01-07 09:05:37 +0900

    ✨feat: separated PostMorePageTemplate
    Related to: #117

--- src/pages/PostMorePage/PostMorePage.tsx
+++ src/pages/PostMorePage/PostMorePage.tsx
@@ -1,10 +1,8 @@
-import { Box, Typography } from '@mui/material';
-import InfiniteScrollComponent from '@components/commons/InfiniteScroll';
-import PostCard from '@components/commons/DetailPagePostCard';
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useGetPaginatedPostsQuery } from '@features/BookDetailPage/api/postApi';
 import { BookDetailPost } from '@shared/types/type';
+import PostMoreTemplate from '@components/PostMorePage/PostMoreTemplate';
 
 const PostMorePage = (): JSX.Element => {
   const location = useLocation();
@@ -34,19 +32,12 @@ const PostMorePage = (): JSX.Element => {
   };
 
   return (
-    <Box sx={{ padding: '1rem', maxWidth: '1200px', margin: 'auto' }}>
-      <Typography variant="h5" fontWeight="bold" sx={{ marginBottom: '1rem' }}>
-        {`포스트 목록 (${data?.totalPosts || 0}개)`}
-      </Typography>
-
-      <InfiniteScrollComponent
-        items={posts}
-        hasMore={hasMore}
-        fetchMore={fetchMoreData}
-        gridSize={{ xs: 12, md: 6 }}
-        renderItem={(post) => <PostCard {...post} />}
-      />
-    </Box>
+    <PostMoreTemplate
+      totalPosts={data?.totalPosts}
+      posts={posts}
+      hasMore={hasMore}
+      fetchMoreData={fetchMoreData}
+    />
   );
 };
 
