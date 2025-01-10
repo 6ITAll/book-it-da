d6c1030b95ebb9a96f2b4db55a9c17fab5727ab2
Hyun Sang <pycandyq@naver.com>
2025-01-10 12:08:17 +0900

    feat: Add msw mockup userId value - userId 값 추가하여 아바타 클릭시 userId에 맞게 이동하도록 구현

--- src/mocks/handlers/DetailPageReview.ts
+++ src/mocks/handlers/DetailPageReview.ts
@@ -3,6 +3,7 @@ import { http, HttpResponse } from 'msw';
 const reviews = [
   {
     itemId: '40869703',
+    userId: 'testId',
     username: 'Lovely ChaeChae',
     date: '2024.08.01',
     content: '새롭네요!',
@@ -11,6 +12,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test2Id',
     username: '독서왕난이',
     date: '2024.02.27',
     content: '도슨트북 새롭고 재미있어요',
@@ -19,6 +21,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test3Id',
     username: '다비다나고양이',
     date: '2024.10.16',
     content: '책에 더 흥미를 갖게 도와주는 것 같아요',
@@ -27,6 +30,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test4Id',
     username: '책벌레준이',
     date: '2024.09.15',
     content: '정말 좋은 책이에요.',
@@ -35,6 +39,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test5Id',
     username: '책읽는요정',
     date: '2024.01.10',
     content: '이 책은 정말 감동적이었어요!',
@@ -43,6 +48,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test6Id',
     username: '감성독서러',
     date: '2024.03.12',
     content: '읽으면서 많은 깨달음을 얻었습니다.',
@@ -51,6 +57,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test7Id',
     username: '독서초보',
     date: '2024.05.08',
     content: '처음 읽는 책인데 너무 좋았어요.',
@@ -59,6 +66,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test8Id',
     username: '책덕후',
     date: '2024.07.20',
     content: '강력 추천합니다!',
@@ -67,6 +75,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test9Id',
     username: '리뷰왕',
     date: '2024.06.25',
     content: '리뷰를 안 쓸 수가 없을 만큼 좋은 책이에요.',
@@ -75,6 +84,7 @@ const reviews = [
   },
   {
     itemId: '40869703',
+    userId: 'test10Id',
     username: '독서러버',
     date: '2024.04.18',
     content: '책을 통해 많은 것을 배웠습니다.',
@@ -83,6 +93,7 @@ const reviews = [
   },
   {
     itemId: '278770576',
+    userId: 'test11Id',
     username: '독서시러시러',
     date: '2024.05.12',
     content: '너무 좋은 책입니다.',
