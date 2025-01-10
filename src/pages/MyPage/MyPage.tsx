80039df6d8aecd9034803a023129590b64920da9
ph0109 <pf6350@gmail.com>
2025-01-10 10:16:16 +0900

    ✨feat: Enhance MyPage component with dynamic user data loading - URL 파라미터, 로컬 스토리지, 카카오 로그인 정보를 활용한 사용자 정보 로딩 로직 추가 Related to: #134

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,16 +1,75 @@
 import TabSection from '@components/MyPage/TabSection';
 import UserInfoSection from '@components/MyPage/UserInfoSection';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { follows } from 'src/mocks/handlers/follow';
+
+interface UserInfo {
+  userId: string;
+  name: string;
+  avartarUrl: string;
+  about?: string;
+  userStats?: Array<{ count: number; label: string; isAction?: boolean }>;
+}
 
 const MyPage = (): JSX.Element => {
-  /* TODO 로그인 작업 완료 후 내 유저 ID가 기본값으로 들어가도록 확장 */
-  const { userId = 'test' } = useParams();
+  const { userId: urlUserId } = useParams<{ userId: string }>();
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  useEffect(() => {
+    //url로 userId 받기
+    const fetchUserInfo = () => {
+      if (urlUserId) {
+        const followUser = follows.find((user) => user.userId === urlUserId);
+        if (followUser) {
+          setUserInfo(followUser);
+          return;
+        }
+      }
+
+      // 일반 로그인
+      const storedUserInfo = localStorage.getItem('userInfo');
+      if (storedUserInfo) {
+        const users = JSON.parse(storedUserInfo);
+        if (users.length > 0) {
+          setUserInfo(users[0]);
+          return;
+        }
+      }
+
+      // 카카오 로그인
+      const kakaoUserInfo = localStorage.getItem('kakaoUserInfo');
+      if (kakaoUserInfo) {
+        const kakaoUser = JSON.parse(kakaoUserInfo);
+        setUserInfo({
+          userId: kakaoUser.id.toString(),
+          name: kakaoUser.properties.nickname,
+          avartarUrl: kakaoUser.properties.profile_image,
+        });
+        return;
+      }
+
+      setUserInfo(null);
+    };
+
+    fetchUserInfo();
+  }, [urlUserId]);
+
+  if (!userInfo) {
+    return (
+      <Container maxWidth="md">
+        <Typography>
+          사용자 정보를 찾을 수 없습니다. 로그인 후 다시 시도해주세요.
+        </Typography>
+      </Container>
+    );
+  }
 
   return (
     <Container maxWidth="md">
-      <UserInfoSection userId={userId} />
-      <TabSection userId={userId} />
+      <UserInfoSection userInfo={userInfo} />
+      <TabSection userId={userInfo.userId} />
     </Container>
   );
 };
