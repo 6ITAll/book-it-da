5fe36871b965e19ea26f001f5db27ed489146581
deenuit113 <leejh5314@gmail.com>
2025-01-11 19:13:29 +0900

    :art:style: modify changes from develop branch vite 설정 변경 알라딘 & 로컬 express 서버 가동 가능하도록

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,23 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({ plugins: [react(), tsconfigPaths()] });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api-docs': {
+          target: 'http://localhost:5174',
+          changeOrigin: true,
+        },
+        '/api': {
+          target: env.VITE_ALADIN_BASEURL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+});
