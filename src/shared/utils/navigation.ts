b6d7d340f78846322eb5de4833a3cf68469d1cf5
Hyun Sang <pycandyq@naver.com>
2025-01-12 20:26:16 +0900

    chore: Changed function to go to common My Page

--- src/shared/utils/navigation.ts
+++ src/shared/utils/navigation.ts
@@ -24,7 +24,7 @@ export const navigateToPostingDetailPage = (
 
 export const navigateToUserPage = (
   navigate: NavigateFunction,
-  userId: string,
+  userId: string | number,
 ) => {
   if (!userId) {
     console.error('Invalid userId:', userId);
