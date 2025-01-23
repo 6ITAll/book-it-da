97eaaf4d9803293ca7dd00595339865306f1e002
deenuit113 <leejh5314@gmail.com>
2025-01-23 17:29:00 +0900

    :fire:style: delete unused files and codes 사용하지 않는 코드 & 파일 삭제 Resolves: #194

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,7 +1,5 @@
 import { bookshelvesHandlers } from './bookshelves';
-// import { feedHandlers } from './feed';
 import { libraryHandlers } from './library';
-// import { postingHandlers } from './posting';
 import { kakaoHandlers } from './kakao';
 import { userHandlers } from './user';
 
@@ -9,19 +7,13 @@ import { genderAgeHandlers } from './genderAge';
 import { reviewHandlers } from './DetailPageReview';
 import { postHandlers } from './DetailPagePost';
 import { addToLibraryHandlers } from './addToLibrary';
-// import { followHandlers } from './follow';
-// import { savedPostingHandlers } from './savePosting';
 export const handlers = [
   ...libraryHandlers,
-  // ...feedHandlers,
   ...bookshelvesHandlers,
   ...postHandlers,
-  // ...postingHandlers,
   ...kakaoHandlers,
   ...userHandlers,
   ...genderAgeHandlers,
   ...reviewHandlers,
   ...addToLibraryHandlers,
-  // ...followHandlers,
-  // ...savedPostingHandlers,
 ];
