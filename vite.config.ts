cfd65fa826701586d8341038c851cbce6e23898f
publdaze <publdaze@naver.com>
2025-01-23 09:53:16 +0900

    ⚡️perf: optimize build by splitting react and react-dom into separate chunks
    Related to: #184

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,14 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom'],
+          },
+        },
+      },
+    },
   };
 });
