a63ff1ade263d42a56238a8a0174d3524e37a164
deenuit113 <leejh5314@gmail.com>
2025-02-02 18:06:10 +0900

    :fire:style: delete useless mock files 사용하지 않는 mock 파일 삭제

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,19 +1,8 @@
-// import { bookshelvesHandlers } from './bookshelves';
-import { libraryHandlers } from './library';
-import { kakaoHandlers } from './kakao';
-import { userHandlers } from './user';
-
 import { genderAgeHandlers } from './genderAge';
 import { reviewHandlers } from './DetailPageReview';
 import { postHandlers } from './DetailPagePost';
-import { addToLibraryHandlers } from './addToLibrary';
 export const handlers = [
-  ...libraryHandlers,
-  // ...bookshelvesHandlers,
   ...postHandlers,
-  ...kakaoHandlers,
-  ...userHandlers,
   ...genderAgeHandlers,
   ...reviewHandlers,
-  ...addToLibraryHandlers,
 ];
