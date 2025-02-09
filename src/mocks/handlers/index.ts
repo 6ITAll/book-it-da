473764e0ce92f8b43e380b129bf10c1efd1d3978
deenuit113 <leejh5314@gmail.com>
2025-02-09 22:27:59 +0900

    :fire:style: delete msw codes msw 관련 코드 삭제

--- src/mocks/handlers/index.ts
+++
@@ -1,8 +0,0 @@
-import { genderAgeHandlers } from './genderAge';
-import { reviewHandlers } from './DetailPageReview';
-import { postHandlers } from './DetailPagePost';
-export const handlers = [
-  ...postHandlers,
-  ...genderAgeHandlers,
-  ...reviewHandlers,
-];
