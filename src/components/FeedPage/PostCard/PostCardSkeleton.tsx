06838bde08912542b041cf9e6ac4afa0f126d9bf
publdaze <publdaze@naver.com>
2025-02-18 14:33:16 +0900

    💄design: adjust relative size in skeleton
    Related to: #242

--- src/components/FeedPage/PostCard/PostCardSkeleton.tsx
+++ src/components/FeedPage/PostCard/PostCardSkeleton.tsx
@@ -15,7 +15,13 @@ const PostCardSkeleton = ({ postType }: PostCardSkeletonProps) => {
         title={<Skeleton height={10} width="80%" style={{ marginBottom: 6 }} />}
         subheader={<Skeleton animation="wave" height={10} width="40%" />}
       />
-      <Skeleton variant="rectangular" width="100%" height={210} />
+      <Skeleton
+        variant="rectangular"
+        width="100%"
+        sx={{
+          height: { xs: 410, sm: 310, md: 275, lg: 210 },
+        }}
+      />
       <Box padding={2}>
         <Skeleton width="60%" />
         <Skeleton width="90%" />
