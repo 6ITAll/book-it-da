1f2a0ea65141de90cf9e7748ed3a98161801357b
deenuit113 <leejh5314@gmail.com>
2025-02-20 14:04:36 +0900

    :fire:refactor: delete useless type 사용하지 않는 타입 삭제 Related to: #269

--- src/shared/types/type.ts
+++ src/shared/types/type.ts
@@ -7,17 +7,6 @@ export type FeedType = '추천' | '팔로워' | '팔로잉';
 // 독서 상태 타입
 export type ReadingStatusType = 'READING' | 'COMPLETED' | 'WISH' | null;
 
-// 책 상세페이지 포스트
-export interface BookDetailPost {
-  isbn: string;
-  userId: string;
-  title: string;
-  description: string;
-  userName: string;
-  avatar: string;
-  createdAt: string;
-}
-
 // 유저 인터페이스
 export interface User {
   id: string;
@@ -25,19 +14,6 @@ export interface User {
   name?: string;
   avatarUrl?: string;
   isFollowing?: boolean;
-}
-
-// UserInfo 인터페이스 정의
-export interface UserInfo {
-  id: string;
-  username: string;
-  name: string;
-  phone: string;
-  gender: string;
-  avatarUrl: string;
-  about: string;
-  age: number | null;
-  email: string;
 }
 
 // 게시물 interface
@@ -72,20 +48,6 @@ export interface SavedPosting {
   updated_at: string;
 }
 
-export interface Review {
-  username: string;
-  date: string;
-  content: string;
-  likes: number;
-  rating: number;
-  userId: string;
-}
-
-// 책 상세 페이지에 아바타에 필요한 userId 확장 타입
-export interface ReviewCard extends Review {
-  userId: string;
-}
-
 // 책 interface
 export interface Book {
   isbn: string;
@@ -99,20 +61,4 @@ export interface SavedBook extends Book {
   bookshelfId: string; // 책장 ID
   addedAt: string; // 책장에 저장된 시간
   readingStatus: ReadingStatusType; // 독서 상태
-}
-
-// 리뷰 데이터 타입
-export interface Review {
-  username: string;
-  date: string;
-  content: string;
-  likes: number;
-  rating: number;
-}
-
-// 책 상세페이지 성별 및 연령 데이터 타입
-export interface GenderAge {
-  age: string;
-  male: number;
-  female: number;
 }
